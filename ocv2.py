1e81a5c74986f0af3e0feedc94722493c9e1a27c
viralsanghavi <vsanghavi3@gmail.com>
2020-07-23 19:04:03 +0530

    Exit Instruction Added

--- ocv2.py
+++ ocv2.py
@@ -1,5 +1,6 @@
 import cv2
 from random import randrange
+from datetime import datetime, date, time, timezone
 
 
 trained_face_daa = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -8,10 +9,18 @@ trained_face_daa = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 webcam = cv2.VideoCapture(0)
 
+exitText = "Long press 'Q' or 'q' to exit"
+capture = "Press 'C' to capture."
+font = cv2.FONT_HERSHEY_SIMPLEX
+text = datetime.now()
+
 
 while True:
     sfr, frame = webcam.read()
     grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.putText(frame, str(exitText), (80, 20), font, 0.82, (200, 255, 255), 2, cv2.LINE_AA)
+    # Add Capture feature
+    #cv2.putText(frame, str(exitText), (80, 20), font, 0.82, (200, 255, 255), 2, cv2.LINE_AA)
 
     face_coordinates = trained_face_daa.detectMultiScale(grayscaled_img)
     for (x, y, w, h) in face_coordinates:
@@ -24,9 +33,10 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+
 webcam.release()
 
-cv2.d
 cv2.destroyAllWindows()
 
 print("Code Completed")
